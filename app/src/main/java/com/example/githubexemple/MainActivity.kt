42f4aee73d622ecc74cc14307ba5c6177caa3fb3
abdelaliichou <97667005+abdelaliichou@users.noreply.github.com>
2023-02-06 16:15:59 +0100

    this is the forth commit !

--- app/src/main/java/com/example/githubexemple/MainActivity.kt
+++ app/src/main/java/com/example/githubexemple/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         print("This message is in the third commit !")
+
+        print("This message is in the forth commit !")
     }
 }
